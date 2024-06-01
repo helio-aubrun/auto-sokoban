2a9a1f574bbcb2ca807efdcd77d43e23b68c316d
victor-duwat <victor.duwat@laplateforme.io>
2024-06-01 14:13:39 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from menu import MainMenu
+
+# Initialisation de Pygame
+pygame.init()
+
+# Paramètres de la fenêtre
+WIDTH, HEIGHT = 1200, 960
+TILESIZE = 40
+
+# Initialisation de la fenêtre
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Sokoban Game")
+
+def main():
+    main_menu = MainMenu(screen, WIDTH, HEIGHT)
+    main_menu.run()
+
+if __name__ == "__main__":
+    main()
